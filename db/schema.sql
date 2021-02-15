4886da3a4fb209a5d5b5e4ebb8a473fc3d964797
Sara Wilson <sarakatewilson98@gmail.com>
2021-02-15 14:19:12 -0500

    adding seeds

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(500) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+);
