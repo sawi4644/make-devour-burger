cb0aa8bac2294d64db779b0ddade26b07e39f97a
Sara Wilson <sarakatewilson98@gmail.com>
2021-02-15 15:29:51 -0500

    adding server.js syntax to run proper files

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
