7279c3cfca2adc4910dbc260b7941a2eec74dcbd
Sara Wilson <sarakatewilson98@gmail.com>
2021-02-15 15:59:28 -0500

    updating burger.js to read through orm and add mysql functions

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,22 @@
-const connection= require("../config/connection")
-const orm= require("../config/orm")
+const orm = require("../config/orm")
 
-const burger={
-    all: function(burg) {
-        orm.selectAll("burgers", function(data) {
-          burg(data);
+const burger = {
+    all: function (burg) {
+        orm.all("burgers", function (result) {
+            burg(result);
         });
-      },
-    //insert
-    //update
-}
+    },
+    
+    insert: function (cols, vals, burg) {
+        orm.create("burgers", cols, vals, function (result) {
+            burg(result);
+        });
+    },
+    update: function (objColVals, condition, burg) {
+        orm.update("burgers", objColVals, condition, function (result) {
+            burg(result);
+        });
+    }
+};
 
-module.exports=burger;
+module.exports = burger;
