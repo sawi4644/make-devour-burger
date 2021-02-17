e4f8e9847dc9a9831b5ceec35907b1ea49da1abc
Sara Wilson <sarakatewilson98@gmail.com>
2021-02-16 22:00:33 -0500

    updating routers inside the burger controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,46 +1,48 @@
-const express= require("express");
-const burger= require("../models/burger");
+const express = require('express')
+const router = express.Router()
+const burger = require('../models/burger')
 
-const router= express.Router();
-
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      var hbsObject = {
-        burger: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+    burger.all(function(data) {
+        let hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject)
+        res.render('index', hbsObject)
+    })
   });
+
+  router.post('/api/burger', function(req, res){
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, false
+    ], function(result){
+      res.json({id: result.insertId})
+    })
+    console.log(req.body)
+  })
+
+  //Update put route with correct sql syntax 
+
+  router.put('/api/burger/:id', function(req,res) {
+    // console.log(req.params.id)
+    let id = req.params.id
+    burger.update([
+      "devoured = true"
+    ], [
+      `id = ${id}` 
+    ], function(result) {
+      res.json({id: result.insertId})
+    })
+  })
+
   
-//   router.post("/api/cats", function(req, res) {
-//     burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
-//       res.json({ id: result.insertId });
-//     });
-//   });
+
+
+
+
+
   
-//   router.put("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
-  
-//     console.log("condition", condition);
-  
-    // burger.update(
-    //   {
-    //     sleepy: req.body.sleepy
-    //   },
-    //   condition,
-    //   function(result) {
-    //     if (result.changedRows === 0) {
-    //       // If no rows were changed, then the ID must not exist, so 404
-    //       return res.status(404).end();
-    //     }
-    //     res.status(200).end();
-  
-    //   }
-//     );
-//   });
-  
-  // Export routes for server.js to use.
+
   module.exports = router;
-  
